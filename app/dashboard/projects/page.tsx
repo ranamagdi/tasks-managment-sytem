a9d8d8ff0fa4cca47c49df0414519f69e3b57c4b
ranamagdi <ranagrida@gmail.com>
2026-05-25 12:26:12 +0300

    fix:the error in build of projects pages

--- app/dashboard/projects/page.tsx
+++ app/dashboard/projects/page.tsx
@@ -1,3 +1,4 @@
+import  { Suspense } from "react";
 import ProjectsPage from "./projects-client";
 
 export const metadata = {
@@ -5,5 +6,9 @@ export const metadata = {
 };
 
 export default function Projects() {
-  return <ProjectsPage />;
+  return (
+    <Suspense fallback={<div />}>
+      <ProjectsPage />
+    </Suspense>
+  );
 }
